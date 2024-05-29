91502d2461b3cefe38fe2833bd3de8094539ece7
Bron Thulke <bron@angelwebdesigns.com.au>
2024-05-29 07:47:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var x = document.getElementById("member");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
